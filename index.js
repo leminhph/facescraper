0d1437a29edc862d2b225729ae1e42319289663b
Le Minh Pham Hoang <minh@Les-MacBook-Pro.local>
2019-11-26 18:07:29 +0700

    wip

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--incognito'],
+    headless: false
+  });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+  await page.goto('https://www.facebook.com/toyotavnsports/posts/669934053429917');
 
-  await browser.close();
+  process.on('SIGTERM', async () => {
+    await browser.close();
+  });
 })();
